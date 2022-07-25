c562dd40308cb7709c2feff6f05cfde8f6cdf506
Nikolay Igotti <igotti@gmail.com>
2022-07-25 19:58:15 +0300

    Fix for payload passing

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ kotlin {
     val hostOs = System.getProperty("os.name")
     val isMingwX64 = hostOs.startsWith("Windows")
     val nativeTarget = when {
-        hostOs == "Mac OS X" -> macosX64("native")
+        hostOs == "Mac OS X" -> macosArm64("native")
         hostOs == "Linux" -> linuxX64("native")
         isMingwX64 -> mingwX64("native")
         else -> throw GradleException("Host OS is not supported in Kotlin/Native.")
