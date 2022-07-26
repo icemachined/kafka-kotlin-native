c3cccb601b9687a3f37d07693559a1a07d4dcd6c
Dmitry Morozovsky <27895587+icemachined@users.noreply.github.com>
2022-07-26 11:53:25 +0300

    revert arch

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,7 @@ kotlin {
     val isMingwX64 = hostOs.startsWith("Windows")
     val nativeTarget = when {
         hostOs == "Mac OS X" -> macosArm64("native")
-        hostOs == "Linux" -> linuxArm64("native")
+        hostOs == "Linux" -> linuxX64("native")
         isMingwX64 -> mingwX64("native")
         else -> throw GradleException("Host OS is not supported in Kotlin/Native.")
     }
