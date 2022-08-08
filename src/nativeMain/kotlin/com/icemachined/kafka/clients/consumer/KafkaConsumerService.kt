df4aa194c3834066f07cac470d4b26faf2076c98
Dmitry Morozovsky <27895587+icemachined@users.noreply.github.com>
2022-08-08 15:15:58 +0300

    consumer

--- src/nativeMain/kotlin/com/icemachined/kafka/clients/consumer/KafkaConsumerService.kt
+++ src/nativeMain/kotlin/com/icemachined/kafka/clients/consumer/KafkaConsumerService.kt
@@ -34,7 +34,7 @@ class KafkaConsumerService<K, V>(
     fun isStopped(): Boolean = !(kafkaPollingJobFuture?.result?.isActive?:false)
     override fun start() {
         kafkaPollingJobFuture = worker.execute(TransferMode.SAFE,
-            { KafkaConsumerJob(config, consumer, _isConsumerPollingActive.freeze(), _isStopped.freeze()) })
+            { KafkaConsumerJob(config.freeze(), consumer.freeze(), _isConsumerPollingActive.freeze(), _isStopped.freeze()) })
         {
             it.pollingCycle()
         }
