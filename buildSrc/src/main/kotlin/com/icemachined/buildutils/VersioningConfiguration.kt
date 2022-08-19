1c369d07fd63176c5908c9a8bc7e62f9725ab249
Dmitry Morozovsky <27895587+icemachined@users.noreply.github.com>
2022-08-19 17:25:56 +0300

    change versioning configuration

--- buildSrc/src/main/kotlin/com/icemachined/buildutils/VersioningConfiguration.kt
+++ buildSrc/src/main/kotlin/com/icemachined/buildutils/VersioningConfiguration.kt
@@ -5,12 +5,15 @@
 package com.icemachined.buildutils
 
 import org.ajoberstar.grgit.Grgit
+import org.ajoberstar.grgit.gradle.GrgitServiceExtension
+import org.ajoberstar.grgit.gradle.GrgitServicePlugin
 import org.ajoberstar.reckon.gradle.ReckonExtension
 import org.ajoberstar.reckon.gradle.ReckonPlugin
 import org.gradle.api.GradleException
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.apply
 import org.gradle.kotlin.dsl.configure
+import org.gradle.kotlin.dsl.getByType
 
 @Suppress("MISSING_KDOC_ON_FUNCTION", "MISSING_KDOC_TOP_LEVEL")
 /**
@@ -20,6 +23,8 @@ import org.gradle.kotlin.dsl.configure
  */
 fun Project.configureVersioning() {
     apply<ReckonPlugin>()
+    apply<GrgitServicePlugin>()
+    val grgitProvider = project.extensions.getByType<GrgitServiceExtension>().service.map { it.grgit }
 
     val isSnapshot = hasProperty("reckon.stage") && property("reckon.stage") == "snapshot"
     configure<ReckonExtension> {
@@ -35,17 +40,15 @@ fun Project.configureVersioning() {
     // to activate release, provide `-Prelease` or `-Prelease=true`. To deactivate, either omit the property, or set `-Prelease=false`.
     val isRelease = hasProperty("release") && (property("release") as String != "false")
     if (isRelease) {
-        val grgit = project.findProperty("grgit") as Grgit  // grgit property is added by reckon plugin
+        val grgit = grgitProvider.get()
         val status = grgit.repository.jgit.status().call()
         if (!status.isClean) {
-            throw GradleException(
-                "Release build will be performed with not clean git tree; aborting. " +
-                        "Untracked files: ${status.untracked}, uncommitted changes: ${status.uncommittedChanges}"
-            )
+            throw GradleException("Release build will be performed with not clean git tree; aborting. " +
+                    "Untracked files: ${status.untracked}, uncommitted changes: ${status.uncommittedChanges}")
         }
     }
     if (isSnapshot) {
-        val grgit = project.findProperty("grgit") as Grgit  // grgit property is added by reckon plugin
+        val grgit = grgitProvider.get()
         // A terrible hack to remove all pre-release tags. Because in semver `0.1.0-SNAPSHOT` < `0.1.0-alpha`, in snapshot mode
         // we remove tags like `0.1.0-alpha`, and then reckoned version will still be `0.1.0-SNAPSHOT` and it will be compliant.
         val preReleaseTagNames = grgit.tag.list()
